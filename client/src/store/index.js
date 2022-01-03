f654397d5da4df9571aacff12a80678c0c9907db
Alexander Koik-Cestone <akoikcestone@gmail.com>
2022-01-02 21:40:02 -0500

    added redux devtools extension to redux store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import loggerMiddleware from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import user from "./user";
 import conversations from "./conversations";
 import activeConversation from "./activeConversation";
@@ -10,14 +12,14 @@ const CLEAR_ON_LOGOUT = "CLEAR_ON_LOGOUT";
 
 export const clearOnLogout = () => {
   return {
-    type: CLEAR_ON_LOGOUT
+    type: CLEAR_ON_LOGOUT,
   };
 };
 
 const appReducer = combineReducers({
   user,
   conversations,
-  activeConversation
+  activeConversation,
 });
 const rootReducer = (state, action) => {
   if (action.type === CLEAR_ON_LOGOUT) {
@@ -27,4 +29,7 @@ const rootReducer = (state, action) => {
   return appReducer(state, action);
 };
 
-export default createStore(rootReducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
+export default createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunkMiddleware, loggerMiddleware))
+);
