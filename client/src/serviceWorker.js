c193fdf0be1826b17b176d4d7c8f6bbbfb97f483
Alexander Koik-Cestone <akoikcestone@gmail.com>
2022-01-03 12:50:23 -0500

    Changed an erroneous console.log to console.error in serviceWorker.js

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -94,7 +94,7 @@ function registerValidSW(swUrl, config) {
       };
     })
     .catch((error) => {
-      console.log("Error during service worker registration:", error);
+      console.error("Error during service worker registration:", error);
     });
 }
 
