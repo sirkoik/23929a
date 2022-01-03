d5e7b0c227dad14f11b5926405ebd599983ced76
Alexander Koik-Cestone <akoikcestone@gmail.com>
2022-01-02 21:18:12 -0500

    Fixed messages sort order

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -6,11 +6,19 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
-  console.error("[Messages]", messages);
+  console.error("[Messages]: messages", messages);
+
+  // sort the messages array by the time the messages were created.
+  // uses localeCompare to keep the sorting internationalization friendly.
+  const orderedMessages = [...messages].sort((a, b) =>
+    a.createdAt.localeCompare(b.createdAt)
+  );
+
+  console.error("[Messages]: orderedMessages", orderedMessages);
 
   return (
     <Box>
-      {messages.map((message) => {
+      {orderedMessages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
