5087f0c873488c359cb42961b8a880957c2f8ac5
Alexander Koik-Cestone <akoikcestone@gmail.com>
2022-01-20 10:24:47 -0500

    added avatar to sender bubble, logic in Messages

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -6,15 +6,21 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
+  // looks for the last read id in a conversation.
+  // the conversation must be sorted ascending by locale date.
+  const lastReadId = messages.reduce((acc, message) => ((message.recipientHasRead && message.senderId === userId) || message.senderId === otherUser.id)? message.id : acc, -1);
+
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
+        const isLastRead = message.id === lastReadId;
+
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble key={message.id} text={message.text} time={time} otherUser={otherUser} isLastRead={isLastRead} />
         ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} isLastRead={message.isLastRead} />
+          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} isLastRead={isLastRead} />
         );
       })}
     </Box>
