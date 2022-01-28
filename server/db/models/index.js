4f101c6542bbdee6a4e01d4654a7f58dfd42ab4e
Alexander Koik-Cestone <akoikcestone@gmail.com>
2022-01-27 21:27:30 -0500

    removed unnecessary comments

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,9 +4,6 @@ const Message = require("./message");
 
 // associations
 
-// many-to-many association: a User can be part of many Conversations.
-// Conversations can have many Users.
-// This removes the two-user-per-conversation limit.
 User.belongsToMany(Conversation, {through: 'userConversations', as: 'conversation'});
 Conversation.belongsToMany(User, {through: 'userConversations', as: 'user'});
 Message.belongsTo(Conversation);
